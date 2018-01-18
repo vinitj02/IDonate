81b18142f1fac859425ed9faf690585699985649
Ashish <ashishmittal2810@gmail.com>
2018-01-18 19:11:23 +0530

    Almost Donate Completed

---
+++ app/src/main/java/com/androidprojects/vinit/idonate/OTPReceiver.java
@@ -0,0 +1,10 @@
+package com.androidprojects.vinit.idonate;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by ashis on 18-01-2018.
+ */
+
+public class OTPReceiver extends BroadcastReceiver {
+}
---
+++ app/src/main/java/com/androidprojects/vinit/idonate/activities/DonateActivity.java
@@ -0,0 +1,10 @@
+package com.androidprojects.vinit.idonate.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ashis on 18-01-2018.
+ */
+
+public class DonateActivity extends AppCompatActivity {
+}
